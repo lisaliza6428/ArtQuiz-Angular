a0264ed9a313b886db096e29d44c258387738532
Elizaveta <elizavetaivanushenko@gmail.com>
2022-05-26 12:33:51 +0300

    feat: add app pages and roiting

--- artquiz/src/app/app-routing.module.ts
+++ artquiz/src/app/app-routing.module.ts
@@ -1,7 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { QuestionPageComponent } from './pages/question-page/question-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'start',
+    pathMatch: 'full',
+  },
+  {
+    path: 'start',
+    pathMatch: 'full',
+    component: StartPageComponent,
+  },
+  {
+    path: 'categories',
+    pathMatch: 'full',
+    component: CategoriesPageComponent,
+  },
+  {
+    path: 'question',
+    pathMatch: 'full',
+    component: QuestionPageComponent,
+  },
+  {
+    path: 'settings',
+    pathMatch: 'full',
+    component: SettingsPageComponent,
+  },
+  {
+    path: '**',
+    component: ErrorPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- artquiz/src/app/app.module.ts
+++ artquiz/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { StartPageComponent } from './pages/start-page/start-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
+import { CategoriesPageComponent } from './pages/categories-page/categories-page.component';
+import { QuestionPageComponent } from './pages/question-page/question-page.component';
+import { ErrorPageComponent } from './pages/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    StartPageComponent,
+    SettingsPageComponent,
+    CategoriesPageComponent,
+    QuestionPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
